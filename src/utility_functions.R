92c02a001a55c9744c5bfe553b5ac6a0b36bbe94
Taylor Sparks Martin <taylorific@gmail.com>
2022-11-03 16:07:45 -0700

    Created utility_functions.R to handle discrete data and site info

---
+++ src/utility_functions.R
@@ -0,0 +1,33 @@
+# Load site data downloaded from Monitoring Portal
+load_site <- function() {
+  fpath <- here("data", "marine_sites.txt")
+  read_tsv(fpath, 
+           col_types = cols(`Site ID` = col_character(), 
+                            `Site Name` = col_character(), 
+                            Locator = col_character(), 
+                            Latitude = col_double(), 
+                            Longitude = col_double(), 
+                            Shallow = col_logical(), 
+                            `Site Type` = col_character(), 
+                            Area = col_character())) %>% 
+    rename(SiteID = `Site ID`, 
+           SiteName = `Site Name`, 
+           SiteType = `Site Type`) %>% 
+    mutate(Shallow = if_else(is.na(Shallow), FALSE, TRUE))
+}
+
+# Update discrete data file w/ all available marine bottle data
+update_discrete <- function() {
+  fpath <- here("data", "discrete_data.csv")
+  site_data <- load_site()
+  sites <- site_data$Locator
+  data("discrete_parms")
+  parms <- discrete_parms$ParmName
+  download_discrete(sites, parms, fpath)
+}
+
+# Load discrete data
+load_discrete <- function() {
+  fpath <- here("data", "discrete_data.csv")
+  import_discrete(fpath)
+}
