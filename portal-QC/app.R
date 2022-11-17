f25d7e714a840f2b97f4d530b7093d0e4d726d82
Taylor Sparks Martin <taylorific@gmail.com>
2022-11-17 11:45:26 -0800

    Working update button and date text

--- portal-QC/app.R
+++ portal-QC/app.R
@@ -11,6 +11,7 @@ library(shiny)
 library(here)
 library(tidyverse)
 library(kcmarine)
+library(ggvis)
 source(here("src", "utility_functions.R"))
 
 # Define UI for application
@@ -19,6 +20,7 @@ ui <- fluidPage(
     # Application title
     titlePanel("Monitoring Portal - Marine QC"),
 
+    # Row of buttons
     fluidRow(
         # BUTTON - refresh downloaded discrete data
         column(2, 
@@ -26,25 +28,41 @@ ui <- fluidPage(
         # TEXT - display date of discrete data file
         column(4, 
                textOutput("date_data"))
-    )
+    ),
     
-    
+    # Tab selections: single site, Central Basin, Whidbey Basin
 )
 
 # Define server logic
 server <- function(input, output) {
-    # Set initial text: date of last data download
+    # Define discrete data path
+    data_fpath <- here("data", "discrete_data.csv")
+    
+    # Define initial data
+    initial_data <- load_discrete()
+    discrete_data <- reactiveValues(data = initial_data)
+    
+    # Define initial data date
+    old_date <- ifelse(file.exists(data_fpath), 
+                       substr(file.info(data_fpath)$mtime, 1, 10), 
+                       "")
+    file_date <- reactiveVal(old_date)
+    
+    # Update data and date text on button push - refresh_data
+    observeEvent(input$refresh_data, {
+        update_discrete()
+        temp <- data.frame(load_discrete())
+        discrete_data$data <- temp
+        
+        new_date <- Sys.Date()
+        file_date(new_date)
+    })
+    
+    # Render data date text
     output$date_data <- renderText(
         paste("Data last updated:", 
-              substr(file.info(here("data", 
-                                    "discrete_data.csv"))$mtime, 
-                     1, 10)))
-    # Update text on button push: date of last data download
-    observeEvent(input$refresh_data,
-                 {update_discrete()
-                     output$date_data <- renderText(
-                         paste("Data last updated:", 
-                               Sys.Date()))})
+              file_date())
+    )
 }
 
 # Run the application 
